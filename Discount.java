d221a805c6a8d92605a97e5a0095bfb6d053e206
Muhammad Ridho <muhammadridhor24@gmail.com>
2023-11-11 13:35:21 +0700

    add interface discount

---
+++ Discount.java
@@ -0,0 +1,13 @@
+package Library_PBO;
+
+/**
+ * Represents a discount interface for books.
+ */
+public interface Discount {
+    /**
+     * Calculates the book's discount.
+     *
+     * @return the discount amount.
+     */
+    public abstract double calculateDiscount();
+}
